a344c4048523e8780b60cb0a5ae8375943625b1a
2Youz <bbhhhfgfg@gmail.com>
2025-05-27 20:35:01 +0700

    Project QLTV

--- DuLieu.py
+++ DuLieu.py
@@ -25,48 +25,6 @@ class Book: #Lớp những quyển sách:
         self.author = data['author']
         self.category = data['category']
         self.quantity = data['quantity']
-class MuonTra:
-    def __init__(self, maKhach, tenKhach, soDienThoai, email, diachi, IDSach, tenSach, ngayMuon, ngayTra, trangthai = False, phat = 0): #trangthai = false là đang mượn, true là trả muộn
-        # Thông tin người mượn
-        self.customerID = maKhach
-        self.customerName = tenKhach
-        self.phone = soDienThoai
-        self.email = email
-        self.address = diachi
-        # Thông tin sách
-        self.bookID = IDSach
-        self.bookName = tenSach
-        # Thông tin mượn trả
-        self.borrowDate = ngayMuon
-        self.returnDate = ngayTra
-        self.status = trangthai
-        self.fine = phat
-    def to_dict(self): #Chuyển đổi đối tượng thành từ điển
-        return {
-            'customerID': self.customerID,
-            'customerName': self.customerName,
-            'phone': self.phone,
-            'email': self.email,
-            'address': self.address,
-            'bookID': self.bookID,
-            'bookName': self.bookName,
-            'borrowDate': self.borrowDate,
-            'returnDate': self.returnDate,
-            'status': self.status,
-            'fine': self.fine
-        } 
-    def from_dict(self, data): #Chuyển đổi từ điển thành đối tượng
-        self.customerID = data['customerID']
-        self.customerName = data['customerName']
-        self.phone = data['phone']
-        self.email = data['email']
-        self.address = data['address']
-        self.bookID = data['bookID']
-        self.bookName = data['bookName']
-        self.borrowDate = data['borrowDate']
-        self.returnDate = data['returnDate']
-        self.status = data['status']
-        self.fine = data['fine']
 class ThuThu: #Lớp những thủ thư:
     def __init__(self, maTT, tenTT, soDT, email, diachi, caLam, luong):
         self.staffID = maTT
